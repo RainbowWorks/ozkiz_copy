9f53ffcc5ee3d8daca47c6516b5dc91b5e25b5f8
RainbowWorks <kuj4235@kakao.com>
2025-05-27 16:21:22 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,38 @@ $(function () {
         $('#header .hd_wrap h1').toggleClass('on')
     })
 });
+
+$(function () {
+    const main_new_slide = new Swiper('.main_new_slide', {
+        loop: true,
+        slidesPerView: 4,
+        spaceBetween: 30,
+        pagination: {
+            el: '.main_new_slide .page',
+            clickable: true,
+            renderBullet: function (index, className) {
+                return '<span class="' + className + '">' + (index + 1) + '</span>';
+            }
+        },
+        breakpoints: {
+            // 화면 너비가 1024px 이하일 때
+            1442: {
+                slidesPerView: 4,
+                spaceBetween: 30,
+            },
+            // 화면 너비가 768px 이하일 때
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 15,
+            },
+            // 화면 너비가 480px 이하일 때
+            480: {
+                slidesPerView: 2,
+                spaceBetween: 10,
+            },
+            0: {
+                slidesPerView: 1,
+            },
+        }
+    });
+});
